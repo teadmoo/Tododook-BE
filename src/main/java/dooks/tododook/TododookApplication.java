9fd1409bade5415d3ac74f7e47134c74360e10d0
sor999 <sorsor999@naver.com>
2024-05-18 11:50:46 +0900

    feat: 초기 DB 설정

---
+++ src/main/java/dooks/tododook/TododookApplication.java
@@ -0,0 +1,13 @@
+package dooks.tododook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TododookApplication.class, args);
+	}
+
+}
